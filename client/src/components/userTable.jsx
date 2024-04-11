781fbea833881681b83002f2fca22278c606a17f
Caleb Seña Melo <calebsenm@gmail.com>
2024-04-10 23:33:21 -0500

    Diseño de los datos listo

--- client/src/components/userTable.jsx
+++ client/src/components/userTable.jsx
@@ -5,23 +5,23 @@ import style from "@/components/estilos.module.css"
 
 export default function Dato({ empresa }) {
     return (
-        <div className={style.table} style={{ display: 'flex', flexDirection: 'row', padding: '10px', backgroundColor: '#f8f9fa', borderRadius: '5px' }}>
-            <div className={style.datos} style={{ marginRight: '20px' }}>
-                <p style={{ margin: '5px 0' }}> {empresa.id}  </p>
-                <p style={{ margin: '5px 0' }}> {empresa.nombre} </p>
-                <p style={{ margin: '5px 0' }}> {empresa.direccion} </p>
-                <p style={{ margin: '5px 0' }}> {empresa.nit} </p>
-                <p style={{ margin: '5px 0' }}> {empresa.correo} </p>
+        <div className={style.table}>
+            <div className={style.datos}>
+                <p className={style.mismaClase}> {empresa.id} </p>
+                <p className={style.mismaClase}> {empresa.nombre} </p>
+                <p className={style.mismaClase}> {empresa.direccion} </p>
+                <p className={style.mismaClase}> {empresa.nit} </p>
+                <p className={style.mismaClase}> {empresa.correo} </p>
+
+
             </div>
 
-            <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center' }}>
-                <button style={{ marginBottom: '10px', padding: '5px 10px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }}> Borrar </button>
-                <button style={{ padding: '5px 10px', backgroundColor: '#28a745', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }}> Editar </button>
+            <div className={style.botonDiv}>
+                <button style={{ backgroundColor: "green" }} className={style.boton}> Borrar </button>
+                <button style={{ backgroundColor: "blue" }} className={style.boton}> Editar </button>
             </div>
+
         </div>
-
-
-
     );
 }
 

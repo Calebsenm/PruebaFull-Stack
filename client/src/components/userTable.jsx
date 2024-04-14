5e4829f343aca8f704abef1cae5261d506f9a9b1
Calebsenm <calebgamercraft@gmail.com>
2024-04-13 23:37:50 -0500

    OK

--- client/src/components/userTable.jsx
+++ client/src/components/userTable.jsx
@@ -5,18 +5,16 @@ import styles from "@/components/estilos.module.css"
 import { useState } from "react";
 import Modal from "./Modal";
 
-
 function deleteEmp() {
-    alert("hola");
-}
+    alert("Empresa Borrada");
 
-function editEmp() {
-    alert("hola");
+
 }
 
 export default function Dato({ empresa }) {
 
     const [showModal, setShowModal] = useState(false);
+    
 
     return (
         <div className={styles.table}>
@@ -30,6 +28,8 @@ export default function Dato({ empresa }) {
 
             <div className={styles.botonDiv}>
                 <button onClick={deleteEmp} style={{ backgroundColor: "green" }} className={styles.boton}> Borrar </button>
+                
+                
                 <button onClick={
                     () => setShowModal(true)
                 } style={{ backgroundColor: "blue" }} className={styles.boton}> Editar </button>

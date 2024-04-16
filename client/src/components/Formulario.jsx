5ab1cb78c890e914db91bec59b8612870701bb54
Calebsenm <calebgamercraft@gmail.com>
2024-04-16 13:15:39 -0500

    Validacion del Formulario

--- client/src/components/Formulario.jsx
+++ client/src/components/Formulario.jsx
@@ -2,11 +2,58 @@
 "use client"
 
 import styles from "@/components/estilos.module.css"
-import React, { useState, useEffect } from 'react'; 
+import React, { useState, useEffect } from 'react';
 
 function Formulario() {
 
-    const [name, setName] = useState(''); 
+    const [nombre, setNombre] = useState('');
+    const [direccion, setDireccion] = useState('');
+    const [nit, setNit] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [email, setEmail] = useState('');
+
+    const [errors, setErrors] = useState({});
+    const [isFormValid, setIsFormValid] = useState(false);
+
+
+    useEffect(() => {
+        validateForm();
+    }, [nombre, direccion, nit, telefono, email]);
+
+    const validateForm = () => {
+        let errors = {};
+
+        if (!nombre) {
+            errors.nombre = 'El nombre es requerido';
+        }
+        if (!direccion) {
+            errors.direccion = 'La dirección es requerida';
+        }
+        if (!nit) {
+            errors.nit = 'El NIT es requerido';
+        }
+        if (!telefono) {
+            errors.telefono = 'El teléfono es requerido';
+        }
+
+        if (!email) {
+            errors.email = 'Email is required.';
+
+        } else if (!/\S+@\S+\.\S+/.test(email)) {
+            errors.email = 'Email is invalid.';
+        }
+
+        setErrors(errors);
+        setIsFormValid(Object.keys(errors).length === 0);
+    }
+
+    const handleSubmit = () => {
+        if (isFormValid) {
+            console.log('Form submitted successfully!');
+        } else {
+            console.log('Form has errors. Please correct them.');
+        }
+    };
 
     return (
         <main className={styles.main}>
@@ -14,27 +61,64 @@ function Formulario() {
                 <h1>Formulario De Registro </h1>
                 <form id="Formulario" >
                     <div >
-                        <label > Nombre de la empresa </label>
-                        <input type="text" className={styles.inputs}></input>
+                        <input
+                            className={styles.inputs}
+                            placeholder="Nombre de la empresa"
+                            value={nombre}
+                            onChange={(e) => setNombre(e.target.value)}
+                        ></input>
+                        {errors.nombre && <p >{errors.nombre}</p>}
+
                     </div>
                     <div>
-                        <label> Dirección </label>
-                        <input type="text" className={styles.inputs}></input>
+                        <input
+                            className={styles.inputs}
+                            placeholder="Direccion"
+                            value={direccion}
+                            onChange={(e) => setDireccion(e.target.value)}
+                        ></input>
+                        {errors.direccion && <p >{errors.direccion}</p>}
+
                     </div>
                     <div>
-                        <label> NIT </label>
-                        <input type="text" className={styles.inputs}></input>
+                        <input
+                            className={styles.inputs}
+                            placeholder="Nit"
+                            value={nit}
+                            onChange={(e) => setNit(e.target.value)}
+                        ></input>
+                        {errors.nit && <p >{errors.nit}</p>}
+
+                    </div>
+
+                    <div>
+                        <input
+                            className={styles.inputs}
+                            placeholder="Telefono"
+                            value={telefono}
+                            onChange={(e) => setTelefono(e.target.value)}
+                        ></input>
+                        {errors.telefono && <p >{errors.telefono}</p>}
+
+                    </div>
+                  
+                    <div>
+                        <input
+                            className={styles.inputs}
+                            placeholder="Correo"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        ></input>
+                        {errors.email && <p >{errors.email}</p>}
+
                     </div>
                     <div>
-                        <label>Teléfono </label>
-                        <input type="text" className={styles.inputs}></input>
-                    </div>
-                    <div>
-                        <label>Email </label>
-                        <input type="text" className={styles.inputs}></input>
-                    </div>
-                    <div>
-                        <button className={styles.inputsSub}> Guardar </button>
+                        <button 
+                            className={styles.inputsSub} 
+                            disabled={!isFormValid} 
+                            onClick={handleSubmit} 
+
+                        > Guardar </button>
                     </div>
                 </form>
             </div>
@@ -45,4 +129,3 @@ function Formulario() {
 }
 
 export default Formulario;
-

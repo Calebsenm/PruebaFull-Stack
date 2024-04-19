b70b0da729cdcc35e8f96ec5aff59d9fdc6d3902
Caleb Seña Melo <calebsenm@gmail.com>
2024-04-19 10:32:00 -0500

    Agregando comntario a api

--- server/src/main.py
+++ server/src/main.py
@@ -143,3 +143,16 @@ if __name__ == "__main__":
     app.run(debug=True)
 
 
+
+
+
+"""
+Las rutas usasas en el proyecto son:
+
+- /api/empresas
+    con los los metodos post get  
+
+/api/empresas/<id>
+    con los metodos get put delete
+
+"""
